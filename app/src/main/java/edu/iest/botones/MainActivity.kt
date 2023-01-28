9ffc022fc4c1e5ecafee4dca4dc449ff9254d8ed
GerardoValencia <gerardo.valencia@iest.edu.mx>
2023-01-27 19:03:37 -0600

    third commit

--- app/src/main/java/edu/iest/botones/MainActivity.kt
+++ app/src/main/java/edu/iest/botones/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.iest.botones
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -8,17 +9,29 @@ import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvFrase : TextView
+    private lateinit var etFrase : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var tvFrase : TextView
         tvFrase = findViewById(R.id.textViewFrase)
-        var etFrase : EditText
         etFrase = findViewById(R.id.editTextFrase)
         var bnMostrar = findViewById<Button>(R.id.buttonMostrar)
         bnMostrar.setOnClickListener{
-            val frase = etFrase.text.toString()
+            var frase = getFrase()
             Snackbar.make(tvFrase, "Su frase fue $frase", Snackbar.LENGTH_LONG).show()
         }
+        var bnSiguiente = findViewById<Button>(R.id.bnSiguiente)
+        bnSiguiente.setOnClickListener{
+            var i = Intent(this, SegundaActivity::class.java)
+            i.putExtra("Frase", getFrase())
+            i.putExtra("Edad", 21)
+            startActivity(i)
+            finish()
+        }
+    }
+    private fun getFrase() : String{
+        return etFrase.text.toString()
     }
 }
